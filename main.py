0f637c5a2224c4b53d2654d6b72ac73d5ad742d9
Emanuel Schemberger <emanueldev24@gmail.com>
2022-02-09 14:30:29 -0300

    tweet_post

--- main.py
+++ main.py
@@ -23,50 +23,43 @@ class UserBase(BaseModel):
     user_id: UUID = Field(...)
     email: EmailStr = Field(...)
 
-
 class UserLogin(UserBase):
     password: str = Field(
-            ...,
-            min_length = 8,
-            max_length = 64 
-            )
-
+        ..., 
+        min_length=8,
+        max_length=64
+    )
 
 class User(UserBase):
     first_name: str = Field(
-            ...,
-            min_length = 1,
-            max_length = 50
-            )
-
+        ...,
+        min_length=1,
+        max_length=50
+    )
     last_name: str = Field(
-            ...,
-            min_length = 1,
-            max_length = 50
-            )
-
-    bith_date: Optional[date] = Field(default = None)
-
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
 
 class UserRegister(User):
     password: str = Field(
-            ...,
-            min_length = 8,
-            max_length = 64
-            )
-
+        ..., 
+        min_length=8,
+        max_length=64
+    )
 
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)
     content: str = Field(
-            ...,
-            min_length = 1,
-            max_length = 256
-            )
+        ..., 
+        min_length=1, 
+        max_length=256
+    )
     created_at: datetime = Field(default=datetime.now())
-    update_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
-
 
 # Path Operations
 
@@ -83,9 +76,7 @@ class Tweet(BaseModel):
 def signup(user: UserRegister = Body(...)): 
     """
     Signup
-
     This path operation register a user in the app
-
     Parameters: 
         - Request body parameter
             - user: UserRegister
@@ -95,9 +86,9 @@ def signup(user: UserRegister = Body(...)):
         - email: Emailstr
         - first_name: str
         - last_name: str
-        - birth_date: str
+        - birth_date: datetime
     """
-    with open('users.json', 'r+', encoding = 'utf-8') as f:
+    with open("users.json", "r+", encoding="utf-8") as f: 
         results = json.loads(f.read())
         user_dict = user.dict()
         user_dict["user_id"] = str(user_dict["user_id"])
@@ -127,14 +118,11 @@ def login():
     summary="Show all users",
     tags=["Users"]
 )
-
 def show_all_users(): 
     """
     This path operation shows all users in the app
-
     Parameters: 
         -
-
     Returns a json list with all users in the app, with the following keys: 
         - user_id: UUID
         - email: Emailstr
@@ -201,8 +189,34 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post(): 
-    pass
+def post(tweet: Tweet = Body(...)): 
+    """
+    Post a Tweet
+    This path operation post a tweet in the app
+    Parameters: 
+        - Request body parameter
+            - tweet: Tweet
+    
+    Returns a json with the basic tweet information: 
+        tweet_id: UUID  
+        content: str    
+        created_at: datetime
+        updated_at: Optional[datetime]
+        by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f: 
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a tweet
 @app.get(
@@ -236,4 +250,3 @@ def delete_a_tweet():
 )
 def update_a_tweet(): 
     pass
-
